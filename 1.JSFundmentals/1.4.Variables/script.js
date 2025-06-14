d1ef239667d9f9b95541e2775efec310a33bfd34
Oleksandr Zadorozhnyi <oo.zadorozhnyi@gmail.com>
2025-06-14 15:51:59 +0300

    Finished 1.4 Variables tasks

---
+++ 1.JSFundmentals/1.4.Variables/script.js
@@ -0,0 +1,16 @@
+// Task 1: Working with variables
+
+let admin, name;
+name = "John"; 
+admin = name; 
+alert(admin); 
+
+// Task 2: Giving the right name
+
+let planetEarth;
+let currentVisitorName;
+
+// Task 3: Uppercase const?
+
+const birthday = '18.04.1982'; 
+const BIRTHDAY = '18.04.1982'; // right one
