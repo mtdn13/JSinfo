7089453c71ec78f4da8d9fd50aeeb9ae035447ac
Oleksandr Zadorozhnyi <oo.zadorozhnyi@gmail.com>
2025-06-16 12:08:19 +0300

    Finished 1.9.Comparisons tasks

---
+++ 1.JS_Fundmentals/1.9.Comparisons/script.js
@@ -0,0 +1,8 @@
+// Task1: Comparisons
+5 > 4 // True
+"apple" > "pineapple" // False ("a" = 97, "p" = 112)
+"2" > "12" // True ("2" = 50, "1" = 49)
+undefined == null // True
+undefined === null // False
+null == "\n0\n" // False
+null === +"\n0\n" // False
