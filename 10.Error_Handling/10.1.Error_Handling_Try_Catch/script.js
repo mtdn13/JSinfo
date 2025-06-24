9f18baeead93d085e278eee0c35439f823b1e66c
Oleksandr Zadorozhnyi <oo.zadorozhnyi@gmail.com>
2025-06-24 18:12:14 +0300

    Added 10.Error module and finished 10.1.Error_Handling_Try_Catch task

---
+++ 10.Error_Handling/10.1.Error_Handling_Try_Catch/script.js
@@ -0,0 +1,19 @@
+// Task 1: Finally or just the code?
+
+/* try {
+  work work
+} catch (err) {
+  handle errors
+} finally {
+  cleanup the working space
+} */
+
+/* try {
+  work work
+} catch (err) {
+  handle errors
+}
+
+cleanup the working space */
+
+// Same unless try..catch is used inside function and that function have a return statement. In this case 2 variant won't work
